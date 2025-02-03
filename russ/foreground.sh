e79902401a4541ec46d9326a7b962f0f1b931de6
Russell Trow <russell@dockleafdigital.com>
2025-02-03 16:04:16 +0000

    Update foreground.sh

--- russ/foreground.sh
+++ russ/foreground.sh
@@ -1,6 +1,10 @@
 # Download and install nvm:
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # Download and install Node.js:
 nvm install 22
 
